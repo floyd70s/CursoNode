9cb9bf6a9be7ea105aba20437354849e0dad1178
claudio perez <claudioperez@claudios-Mac-mini.local>
2019-12-17 18:02:00 -0300

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -96,8 +96,27 @@ function loginUser(req, res) {
         }
     })
 }
+
+function updateUser(req,res){
+    var userId= req.params.id
+    var update= req.body
+
+    User.findByIdAndUpdate(userId,update,(err,userUpdated)=>{
+        if(err){
+            res.status(500).send({message:'Error al actualizar usuario'})
+        }else{
+            if(!userUpdated){
+                res.status(200).send({user:userUpdated})
+            }
+        }
+    })
+}
+
+
+
 module.exports = {
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    updateUser
 }
--- routes/user.js
+++ routes/user.js
@@ -13,5 +13,6 @@ var md_auth= require('../middlewares/authenticated')
 api.get('/probando-controlador',md_auth.ensureAuth,UserController.pruebas)
 api.post('/register',UserController.saveUser)
 api.post('/login',UserController.loginUser)
+api.put('/update-user/:id',md_auth.ensureAuth,UserController.updateUser)
 
 module.exports=api
