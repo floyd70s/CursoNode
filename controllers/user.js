e47fc11692ea65fa40b85b1708407d2bfec00ab7
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-17 23:07:45 -0300

    upload

--- controllers/user.js
+++ controllers/user.js
@@ -96,29 +96,44 @@ function loginUser(req, res) {
         }
     })
 }
+/**
+ * @author CPerez
+ * @param {*} req 
+ * @param {*} res 
+ * @description funcion de actualizacion de datos de usuarios.
+ */
+function updateUser(req, res) {
+    var userId = req.params.id
+    var update = req.body
 
-function updateUser(req,res){
-    var userId= req.params.id
-    var update= req.body
-
-    User.findByIdAndUpdate(userId,update,(err,userUpdated)=>{
-        if(err){
-            res.status(500).send({message:'Error al actualizar usuario'})
-        }else{
-            if(!userUpdated){
-                res.status(404).send({message:'No se ha podido actualizar el usuario'})
-            }else {
-                res.status(200).send({user:userUpdated})
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if (err) {
+            res.status(500).send({ message: 'Error al actualizar usuario' })
+        } else {
+            if (!userUpdated) {
+                res.status(404).send({ message: 'No se ha podido actualizar el usuario' })
+            } else {
+                res.status(200).send({ user: userUpdated })
             }
         }
     })
 }
 
-
+function uploadImage(req, res) {
+    var userId = req.params.id
+    var file_name = 'No subido...'
+    
+    if (req.files) {
+        var file_path = req.files.image.path
+    } else {
+        res.status(200).send({ message: 'No se ha subido ninguna imagen' })
+    }
+}
 
 module.exports = {
     pruebas,
     saveUser,
     loginUser,
-    updateUser
+    updateUser,
+    uploadImage
 }
--- routes/user.js
+++ routes/user.js
@@ -9,10 +9,13 @@ var express= require('express')
 var UserController=require('../controllers/user')
 var api= express.Router();
 var md_auth= require('../middlewares/authenticated')
+var multipart=require('connect-multiparty')
+var md_upload=multipart({uploadDir:'./uploads/users'})
 
 api.get('/probando-controlador',md_auth.ensureAuth,UserController.pruebas)
 api.post('/register',UserController.saveUser)
 api.post('/login',UserController.loginUser)
 api.put('/update-user/:id',md_auth.ensureAuth,UserController.updateUser)
+api.post('/upload-Image-user/:id',[md_auth.ensureAuth,md_upload],UserController.uploadImage)
 
 module.exports=api
