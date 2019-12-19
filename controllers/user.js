15756135e46bad131d3835271f5b849d24d27108
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-18 23:41:14 -0300

    subir imagen

--- controllers/user.js
+++ controllers/user.js
@@ -122,9 +122,25 @@ function updateUser(req, res) {
 function uploadImage(req, res) {
     var userId = req.params.id
     var file_name = 'No subido...'
-    
+  
     if (req.files) {
         var file_path = req.files.image.path
+        var file_split= file_path.split('/')
+        var file_name= file_split[2]
+        var ext_file= file_name.split('.')
+        var file_ext= ext_file[1]
+
+        if (file_ext=='jpg'||file_ext=='gif'||file_ext=='png'){
+            User.findByIdAndUpdate(userId,{image:file_name}, (err,userUpdated)=>{
+                if (!userUpdated) {
+                    res.status(404).send({ message: 'No se ha podido actualizar el usuario' })
+                }else {
+                    res.status(200).send({ user: userUpdated })
+                }
+            })
+        }else{
+            res.status(200).send({ message: 'La extension no es correcta' })
+        } 
     } else {
         res.status(200).send({ message: 'No se ha subido ninguna imagen' })
     }
--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ var express= require('express')
 var UserController=require('../controllers/user')
 var api= express.Router();
 var md_auth= require('../middlewares/authenticated')
+
 var multipart=require('connect-multiparty')
 var md_upload=multipart({uploadDir:'./uploads/users'})
 
