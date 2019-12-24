35827442884b49bda447fd2923dafb134765fd60
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-24 13:11:22 -0300

    save artist

--- routes/artist.js
+++ routes/artist.js
@@ -5,5 +5,6 @@ var api=express.Router()
 var md_auth= require('../middlewares/authenticated')
 
 api.get('/artist',md_auth.ensureAuth,ArtistController.getArtist)
+api.post('/artist',md_auth.ensureAuth,ArtistController.saveArtist)
 
 module.exports=api
