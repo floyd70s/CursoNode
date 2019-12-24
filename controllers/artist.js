8ff2e12ac171348e80b034366cdb17cfc5774bfb
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-24 01:15:34 -0300

    save artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -11,6 +11,30 @@ function getArtist(req,res){
     res.status('200').send({message:'metodo getArtist del controlador artist.js'})
 }
 
+function saveArtist(req,res){
+    var artist = new Artist()
+    var params = req.body
+
+    artist.name= params.name
+    artist.description= params.description
+    artist.image= 'null'
+
+    artist.save((err, artistStored)=>{
+        if(err){
+            res.status(500).send({message:'Error al guardar el artista'})
+        }else{
+            if(!artistStored){
+                res.status(404).send({message:'El artista no ha sido guardado'})
+            }else{
+                res.status(200).send({artist:artistStored})
+            }
+        }
+
+    })
+
+}
+
 module.exports={
-    getArtist
+    getArtist,
+    saveArtist
 }
