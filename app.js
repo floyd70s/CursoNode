07de569248f01086965fbabd756140ae51b795d8
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-24 00:53:27 -0300

    module artist

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var app=express();
 
 //cargar rutas
 var user_routes= require('./routes/user')
+var artist_routes= require('./routes/artist')
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -14,6 +16,7 @@ app.use(bodyParser.json());
 
 //rutas base
 app.use('/api',user_routes);
+app.use('/api',artist_routes);
 
 app.get('/pruebas',function(req,res){
     res.status(200).send({message:'Bienvenido al curso'})
