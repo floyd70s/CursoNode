c740f719b27937db9a342efe211a98a8ac764171
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-28 13:24:52 -0300

    borrar

--- controllers/artist.js
+++ controllers/artist.js
@@ -2,15 +2,53 @@
 
 var path= require('path')
 var fs = require('fs')
+var mogoosePaginate=require('mongoosePagination')
 
 var Artist= require('../models/artist')
 var Album= require('../models/album')
 var Song= require('../models/song')
 
-function getArtist(req,res){
-    res.status('200').send({message:'metodo getArtist del controlador artist.js'})
+function getArtists(req,res){
+    var page= req.params.page
+    var itemsPerPage=3
+
+    artist.find().sort('name').paginate(page,itemsPerPage,funcion(err,artists,total){
+        if(err){
+            res.status(500).send({message:'error en la peticion'})
+        }else{
+            if(!artists){
+                res.status(404).send({message:'no hay artistas'})
+            }else{
+                return res.status(200).send({
+                    pages:total,
+                    artists:artists
+                })
+            }
+        }
+    })
 }
 
+function getArtist(req,res){
+    var artistId=req.params.id
+    Artist.findById(artistId,(err,artist)=>{
+        if(err){
+            res.status(500).send({message:"Error en la peticion"})
+        }else{
+            if(!artist){
+                res.status(404).send({message:"El artista no existe"})
+            }else{
+                res.status(200).send({artist})
+            }
+        }
+    })
+}
+
+/**
+ * @author CperezD
+ * @param {} req 
+ * @param {*} res 
+ * @description funcion para guardar el artista
+ */
 function saveArtist(req,res){
     var artist = new Artist()
     var params = req.body
@@ -36,5 +74,5 @@ function saveArtist(req,res){
 
 module.exports={
     getArtist,
-    saveArtist
+    saveArtist  
 }
--- routes/artist.js
+++ routes/artist.js
@@ -4,7 +4,7 @@ var ArtistController= require('../controllers/artist')
 var api=express.Router()
 var md_auth= require('../middlewares/authenticated')
 
-api.get('/artist',md_auth.ensureAuth,ArtistController.getArtist)
+api.get('/artist/:id',md_auth.ensureAuth,ArtistController.getArtist)
 api.post('/artist',md_auth.ensureAuth,ArtistController.saveArtist)
 
 module.exports=api
