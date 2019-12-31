52bba5d5f483d00865c6568329bbcee2b1c9c646
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2019-12-30 23:16:07 -0300

    client -user

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {User} from './models/user'
+import {UserService} from './services/user.service'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', 
+  providers:[UserService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  public title = 'Musify';
+  public user: User
+  public identity
+  public token
+
+  constructor(
+    private _userService:UserService
+  ){
+    this.user= new User('','','','','','ROLE_USER','')
+  }
+
+  ngOnInit(){
+  }
+
+  public onSubmit(){
+    console.log(this.user)
+    this._userService.signup(this.user).subscribe(
+      response => {
+        console.log(response)
+      },
+      error=>{
+        var errorMessage=<any>error
+        if(errorMessage!=null){
+          console.log(error)
+        }
+      }
+    )
+  }
+
 }
---
+++ client/src/app/services/global.ts
@@ -0,0 +1,5 @@
+export var GLOBAL={
+    url:'http://localhost:3977/api/',
+    ip:'127.0.0.1'
+}
+
