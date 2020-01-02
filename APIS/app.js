c8552d73f018d41e9551cbf9f333654a37161ef9
claudio perez <claudioperez@claudios-Mac-mini.local>
2020-01-02 11:37:17 -0300

    APIS completes

--- APIS/app.js
+++ APIS/app.js
@@ -20,7 +20,7 @@ app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next
+    next()
 })
 
 //rutas base
