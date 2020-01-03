a539a9d6138e5d6c1fb416eb1430348e292b4994
Claudio Perez <claudioperez@MacBook-Pro-de-Claudio.local>
2020-01-02 21:27:05 -0300

    fix error version

--- APIS/app.js
+++ APIS/app.js
@@ -15,33 +15,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //cabeceras http 
-<<<<<<< HEAD
-<<<<<<< HEAD
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-=======
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method,');
     res.header('content-type: application/json; charset=utf-8')
->>>>>>> fix headers
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next()
 })
-<<<<<<< HEAD
-=======
-// app.use((req,res,next)=>{
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//     next
-// })
->>>>>>> client - models created
-=======
->>>>>>> fix headers
 
 //rutas base
 app.use('/api',user_routes);
