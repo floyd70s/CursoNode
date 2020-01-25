4345c7385f8353ac2885f6f4164811aae441dc95
claudio <claudioperezdiaz@gmail.com>
2020-01-25 17:21:36 -0300

    -

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserEditComponent } from './components/user-edit.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
